5585c95e3a0608d826019ada0c33a86a83c9944f
subhashiniv6 <56245022+subhashiniv6@users.noreply.github.com>
2021-12-20 13:30:24 +0530

    Update login.java

--- src/main/java/royal/login.java
+++ src/main/java/royal/login.java
@@ -1,1 +1,13 @@
-a
+package royal;
+
+/**
+ * Hello world!
+ *
+ */
+public class login 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
